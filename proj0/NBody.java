a172e2610471ec6cfa154c7ad5bcac6239b550ad
廖紫伊 <liaoziyi@liaoziyideMacBook-Pro.local>
2021-09-13 21:01:47 -0400

    NBody version two

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -76,11 +76,11 @@ public class NBody{
   StdDraw.pause(10);
   t = t + dt;
   }
-  StdOut.printf("%d\n", planets.length);
+  StdOut.printf("%d\n", pArr.length);
 StdOut.printf("%.2e\n", radius);
-for (int i = 0; i < planets.length; i++) {
+for (int i = 0; i < pArr.length; i++) {
     StdOut.printf("%11.4e %11.4e %11.4e %11.4e %11.4e %12s\n",
-                  planets[i].xxPos, planets[i].yyPos, planets[i].xxVel,
-                  planets[i].yyVel, planets[i].mass, planets[i].imgFileName);
+                  pArr[i].xxPos, pArr[i].yyPos, pArr[i].xxVel,
+                  pArr[i].yyVel, pArr[i].mass, pArr[i].imgFileName);
 }
 }
